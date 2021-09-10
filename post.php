0436fd9d22022b6ab464cfbdf57179fb6d762469
RedouaneJnah98 <redouanejnah2000@gmail.com>
2021-09-10 17:59:36 +0100

    add registration file

--- post.php
+++ post.php
@@ -61,18 +61,24 @@
                 $comment_email = $_POST["comment_email"];
                 $comment_content = $_POST["comment_content"];
 
-                $query = "INSERT INTO comments(comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
-                $query .= "VALUES($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'Unapproved', now())";
+                if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
 
-                $create_comment_query = mysqli_query($connection, $query);
+                    $query = "INSERT INTO comments(comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
+                    $query .= "VALUES($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', 'Unapproved', now())";
 
-                if (!$create_comment_query) {
-                    die('FAILED QUERY' . mysqli_error($connection));
+                    $create_comment_query = mysqli_query($connection, $query);
+
+                    if (!$create_comment_query) {
+                        die('FAILED QUERY' . mysqli_error($connection));
+                    }
+
+                    $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = $the_post_id";
+                    $update_comment_count = mysqli_query($connection, $query);
+                } else {
+                    echo "<script>alert('Error! field cannot be empty!')</script>";
                 }
-
-                $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = $the_post_id";
-                $update_comment_count = mysqli_query($connection, $query);
             }
+
             ?>
 
             <!-- Comments Form -->
