4c2a3fb015e1b17a87767d46bda07e313404d794
RedouaneJnah98 <redouanejnah2000@gmail.com>
2021-09-20 22:33:31 +0100

    Join posts table & categories table

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -97,7 +97,10 @@ if (isset($_POST["checkboxArray"])) {
         <tbody>
 
             <?php
-            $query = "SELECT * FROM posts ORDER BY post_id DESC";
+            // $query = "SELECT * FROM posts ORDER BY post_id DESC";
+
+            $query = "SELECT posts.post_id, posts.post_title, posts.post_author, posts.post_user, posts.post_category_id, posts.post_status, posts.post_image, posts.post_tags, posts.post_comment_count, posts.post_date, posts.post_views_count, categories.cat_id, categories.cat_title FROM posts LEFT JOIN categories ON posts.post_category_id = categories.cat_id ORDER BY posts.post_id DESC ";
+
             $select_posts = mysqli_query($connection, $query);
 
             while ($row = mysqli_fetch_assoc($select_posts)) {
@@ -112,6 +115,8 @@ if (isset($_POST["checkboxArray"])) {
                 $post_comment_count = $row["post_comment_count"];
                 $post_date = $row["post_date"];
                 $post_views_count = $row["post_views_count"];
+                $category_id = $row['cat_id'];
+                $category_title = $row["cat_title"];
 
                 echo "<tr>";
             ?>
@@ -126,15 +131,15 @@ if (isset($_POST["checkboxArray"])) {
                     echo "<td>$post_user</td>";
                 }
 
-                $query = "SELECT * FROM categories WHERE cat_id = $post_category_id";
-                $select_category = mysqli_query($connection, $query);
+                // $query = "SELECT * FROM categories WHERE cat_id = $post_category_id";
+                // $select_category = mysqli_query($connection, $query);
 
-                while ($row = mysqli_fetch_array($select_category)) {
-                    $cat_id = $row["cat_id"];
-                    $cat_title = $row["cat_title"];
+                // while ($row = mysqli_fetch_array($select_category)) {
+                //     $cat_id = $row["cat_id"];
+                //     $cat_title = $row["cat_title"];
 
-                    echo "<td>$cat_title</td>";
-                }
+                echo "<td>$category_title</td>";
+                // }
 
                 echo "<td>$post_status</td>";
                 echo "<td><img src='../images/$post_image' width='100' alt='image'/></td>";
